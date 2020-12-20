48fd4eb93b3b150484ddc5453ac75f25400ab950
panduka wijekoon <42469227+panduka1996@users.noreply.github.com>
2020-12-20 13:35:25 +0530

    Add files via upload

---
+++ JwtToken/JwtToken/Controllers/UserCred.cs
@@ -0,0 +1,8 @@
+﻿namespace JwtToken.Controllers
+{
+    public class UserCred
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ JwtToken/JwtToken/IJwtAuthenticationManager.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JwtToken
+{
+    public interface IJwtAuthenticationManager
+    {
+        public string Authenticated(string username, string password);
+    }
+}
